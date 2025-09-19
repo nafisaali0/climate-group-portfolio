7f71dcaf5c72c16fe7a995ea8337fdb00a370e88
nafisaali0 <nafisaali20006@gmail.com>
2025-09-19 08:35:15 +0600

    change link

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,5 +1,6 @@
 import { BiSolidCategory } from "react-icons/bi"
 import logo from "../../assets/logo/logo_1.png"
+import projectInfo from "./projectInfo"
 
 
 const Project = () => {
@@ -10,45 +11,50 @@ const Project = () => {
 
                 {/* project */}
                 <div className="flex flex-col gap-5 flex-wrap mt-7">
-
-                    <div
-                        className="w-full bg-dark_green rounded-xl py-3 px-5"
-                    >
-                        <div className="flex items-center justify-between py-3">
-                            <div className="w-auto">
-                                <div className="flex gap-2 items-center">
-                                    <div className="w-auto">
-                                        <div className="avatar">
-                                            <div className="w-16 rounded-xl border border-dark_green">
-                                                <img src={logo} />
+                    {
+                        projectInfo?.map((project, index) =>
+                            <>
+                                <div key={index}
+                                    className="w-full bg-dark_green rounded-xl py-3 px-5"
+                                >
+                                    <div className="flex items-center justify-between py-3">
+                                        <div className="w-auto">
+                                            <div className="flex gap-2 items-center">
+                                                <div className="w-auto">
+                                                    <div className="avatar">
+                                                        <div className="w-16 rounded-xl border border-dark_green">
+                                                            <img src={logo} />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="w-auto p-2 space-y-1">
+                                                    <h3 className="flex items-center gap-1 text-xs font-medium text-textSmallGray">
+                                                        <BiSolidCategory
+                                                            title="category"
+                                                            className="text-white"
+                                                            style={{ width: "12px", height: "12px" }}
+                                                        />
+                                                        <span>none</span>
+                                                    </h3>
+                                                    <a title="See Details" href="">
+                                                        <h2 className="text-sm font-semibold">
+                                                            {project.name}
+                                                        </h2>
+                                                    </a>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div className="w-auto p-2 space-y-1">
-                                        <h3 className="flex items-center gap-1 text-xs font-medium text-textSmallGray">
-                                            <BiSolidCategory
-                                                title="category"
-                                                className="text-white"
-                                                style={{ width: "12px", height: "12px" }}
-                                            />
-                                            <span>none</span>
-                                        </h3>
-                                        <a title="See Details" href="">
-                                            <h2 className="text-sm font-semibold">
-                                                none
-                                            </h2>
-                                        </a>
+                                        {/* <button className='px-3 py-4 rounded-xl hover:border hover:border-button_color font-semibold text-sm lg:text-md'><a href="#projects">See Project</a></button> */}
+
+                                        {/* new */}
+                                        <button className="projectBtn">
+                                            <span>See Project</span>
+                                        </button>
                                     </div>
                                 </div>
-                            </div>
-                            {/* <button className='px-3 py-4 rounded-xl hover:border hover:border-button_color font-semibold text-sm lg:text-md'><a href="#projects">See Project</a></button> */}
-
-                            {/* new */}
-                            <button className="projectBtn">
-                                <span>See Project</span>
-                            </button>
-                        </div>
-                    </div>
+                            </>
+                        )
+                    }
                 </div>
             </div>
         </>
---
+++ src/Components/Project/projectInfo.js
@@ -0,0 +1,10 @@
+const projectInfo = [
+  {
+    name: "mail",
+    category: "",
+    Image: "",
+    link: "",
+  },
+];
+
+export default projectInfo;
